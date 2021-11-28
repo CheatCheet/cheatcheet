58079192ba4b1207e65d9f28b21c384cf734636d
Remy Phan <remy@cobbai.com>
2021-11-29 00:01:58 +0100

    install webpack and tailwindscss and gem avo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import Rails from "@rails/ujs"
 import "channels"
+import 'tailwindcss/tailwind.css'
+import Turbolinks from "turbolinks"
 
 Rails.start()
 Turbolinks.start()
