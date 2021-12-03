b4a347817bb4a248f1b27c0fbe60c99114b43b8e
= <=>
2021-12-04 00:25:56 +0100

    Add validation to the Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-
+  validates :title, length: { minimum: 5 }
+  validates :body, length: { minimum: 5 }
   scope :from_user, ->(id) { where('user_id = ?', id) }
 end
