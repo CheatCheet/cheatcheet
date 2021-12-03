7d830bfc1ae1293195781beeaa8528df3d52ea26
= <=>
2021-12-03 20:23:38 +0100

    Remove SignUp route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
 
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   resources :posts
 end
