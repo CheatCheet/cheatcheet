988740e889a1aee835599a0db9b0749501fbb2a9
= <=>
2021-12-04 00:41:07 +0100

    Show latest posts by default

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,10 @@
 
 class Post < ApplicationRecord
   belongs_to :user
+
   validates :title, length: { minimum: 5 }
   validates :body, length: { minimum: 5 }
+
+  default_scope { order(created_at: :desc) }
   scope :from_user, ->(id) { where('user_id = ?', id) }
 end
