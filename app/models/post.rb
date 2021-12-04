c3105ab7472624a31649ad518813172899362eb8
= <=>
2021-12-04 20:01:52 +0100

    Make CI linter to fail

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,6 @@ class Post < ApplicationRecord
   validates :title, length: { minimum: 5 }
   validates :body, length: { minimum: 5 }
 
-  default_scope { order(created_at: :desc) }
+default_scope { order(created_at: :desc) }
   scope :from_user, ->(id) { where('user_id = ?', id) }
 end
