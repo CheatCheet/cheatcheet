bce1233c9ee2953f527fc61da77e903687d434b5
Remy Phan <remy@cobbai.com>
2021-12-04 23:06:46 +0100

    test and refacto #6

--- app/controllers/devise/omniauth_controller.rb
+++ app/controllers/devise/omniauth_controller.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 module Devise
-  class CallbacksController < OmniauthCallbacksController
+  class OmniauthController < OmniauthCallbacksController
     def github
       @user = User.from_omniauth(request.env['omniauth.auth'])
       sign_in_and_redirect @user
+      # TODO: flash[:error] = 'There was a problem signing you in. Please register or try signing in later.'
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  devise :omniauthable, omniauth_providers: %i[github]
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[github]
 
   validates :email, :pseudo, uniqueness: true
 
