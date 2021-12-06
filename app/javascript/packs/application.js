e49e5a8f89b92b0f1d278f15e056522b39222905
Remy Phan <remy@cobbai.com>
2021-12-07 00:57:14 +0100

    install slim select and front ready

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import * as ActiveStorage from "@rails/activestorage"
-import Rails from "@rails/ujs"
-import "channels"
 import '@hotwired/turbo-rails'
+import * as ActiveStorage from '@rails/activestorage'
+import Rails from '@rails/ujs'
+import 'channels'
 import '../controllers/index'
 
 Rails.start()
