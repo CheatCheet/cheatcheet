b0e8c42ee271ed3d188ed25a9b761a50ab669fad
= <=>
2022-01-09 21:13:46 +0100

    Fix rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,7 @@ class Post < ApplicationRecord
   default_scope { order(created_at: :desc) }
   scope :from_user, ->(id) { where('user_id = ?', id) }
   scope :related_to, lambda { |value|
-                     joins(:action_text_rich_text).where("posts.title ILIKE '%#{value}%' OR
+                       joins(:action_text_rich_text).where("posts.title ILIKE '%#{value}%' OR
                       action_text_rich_texts.body ILIKE '%#{value}%'")
-                   }
+                     }
 end
