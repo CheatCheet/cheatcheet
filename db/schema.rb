381c49765a11c7953f6e97c12b443b350c47bbf4
Remy Phan <remy@cobbai.com>
2022-01-15 22:29:25 +0100

    finalize test

--- db/schema.rb
+++ db/schema.rb
@@ -105,10 +105,8 @@ ActiveRecord::Schema.define(version: 2021_12_18_170559) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
-  add_foreign_key "posts", "stacks"
-  add_foreign_key "posts", "users"
-
   add_foreign_key "bookmarks", "posts"
   add_foreign_key "bookmarks", "users"
+  add_foreign_key "posts", "stacks"
+  add_foreign_key "posts", "users"
 end
--- test/models/bookmark_test.rb
+++ test/models/bookmark_test.rb
@@ -3,7 +3,24 @@
 require 'test_helper'
 
 class BookmarkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'it destroys the bookmark by destroying the post' do
+    assert_changes -> { Bookmark.count }, -1 do
+      bookmarks(:default).post.destroy
+    end
+  end
+  test 'it doesn t create a bookmark for same post and same user' do
+    assert_no_changes -> { Bookmark.count } do
+      default = bookmarks(:default)
+      Bookmark.create(user_id: default.user_id,
+                      post_id: default.post_id)
+    end
+  end
+  test 'it  creates a bookmark' do
+    assert_changes -> { Bookmark.count }, 1 do
+      default = bookmarks(:default)
+      potential_user_ids = User.ids - [default.user_id]
+      Bookmark.create(user_id: potential_user_ids.sample,
+                      post_id: default.post_id)
+    end
+  end
 end
