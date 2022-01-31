848ba094c06695c115335a4a9d38e539eab0a5fb
= <=>
2022-01-31 22:36:59 +0100

    Add votes controller test skeleton

---
+++ test/controllers/posts/votes_controller_test.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class VotesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  test 'it should create an upvote' do
+  end
+
+  test 'it should create a downvote' do
+  end
+
+  test 'it should update a vote' do
+  end
+end
