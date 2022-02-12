cf2c60d7f99d2b32b845bee1e301405038fb3040
Remy Phan <remy@cobbai.com>
2022-02-13 00:57:30 +0100

    restart

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,5 +13,15 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+    def log_as(user_role)
+      user = users(user_role)
+
+      visit new_user_session_url
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: 'password'
+      click_on 'Sign in'
+
+      assert_text 'Signed in successfully.'
+    end
   end
 end
